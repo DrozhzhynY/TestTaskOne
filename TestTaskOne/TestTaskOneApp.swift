d3063eaedc78992ec824a658091e6254ed24a4d5
DrozhzhynY <drozhzhyn.yevhenii@gmail.com>
2022-07-05 20:19:20 +0300

    Initial Commit

---
+++ TestTaskOne/TestTaskOneApp.swift
@@ -0,0 +1,17 @@
+//
+//  TestTaskOneApp.swift
+//  TestTaskOne
+//
+//  Created by admin on 05.07.2022.
+//
+
+import SwiftUI
+
+@main
+struct TestTaskOneApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
